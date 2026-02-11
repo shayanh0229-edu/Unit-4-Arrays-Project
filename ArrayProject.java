fea62ac6da82469cfc64598b1582c42300de7f32
shayanh0229-edu <shayanh@nycstudents.net>
2026-02-10 23:48:51 -0500

    are we cooked?

--- ArrayProject.java
+++ ArrayProject.java
@@ -3,18 +3,27 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.List;
 
+
 public class ArrayProject {
     private String[] hands;
-
+    private int bidValue;
+    private int rank;
+    private String handType;
     private int numberOfUniques;
     private int maxFrequency;
+    private int bracketThingy;
 
-    public ArrayProject(String[] hands) {
+
+
+    public ArrayProject(String[] hands, int bracketThingy) {
+        this.bracketThingy = bracketThingy;
         this.hands = hands;
+        this.rank = 0;
         List<String> list = Arrays.asList(hands);
         Set<String> uniqueStrings = new HashSet<>(list);
         this.numberOfUniques = uniqueStrings.size();
         System.out.println(numberOfUniques);
+
 
         int[] frequencies = new int[numberOfUniques];
         int whichFrequency = 0;
@@ -32,141 +41,109 @@ public class ArrayProject {
                 maxFrequency = frequencies[i];
             }
         }
-    }
-        public int getNumberOfUniques() {
-            return numberOfUniques;
+        if (numberOfUniques == 1) {
+            handType = "fiveOfAKind";
+        } else if (numberOfUniques == 2) {
+            if (maxFrequency == 4) {
+                handType = "fourOfAKind";
+            } else {
+                handType = "fullHouse";
+            }
+        } else if (numberOfUniques == 3) {
+            if (maxFrequency == 3) {
+                handType = "threeOfAKind";
+            } else {
+                handType = "twoPair";
+            }
+        } else if (numberOfUniques == 4) {
+            handType = "onePair";
+        } else if (numberOfUniques == 5) {
+            handType = "highCard";
         }
 
 
-        public int getMaxFrequency() {
-            return maxFrequency;
 
-        }
+
+    }
+    public int getNumberOfUniques() {
+        return numberOfUniques;
     }
 
+    public String getHandType() {
+        return handType;
+    }
+
+    public int getMaxFrequency() {
+        return maxFrequency;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getCardValue(String card) {
+        return switch (card) { //for this part, i had to look to the internet to find a way to
+            case "Ace"   -> 14; // "index" each card to make the comparisons seamless
+            case "King"  -> 13;
+            case "Queen" -> 12;
+            case "Jack"  -> 11;
+            default      -> Integer.parseInt(card);
+        };
+    }
+    public int bidValue(){
 
 
+        return bidValue;
+    }
 
+    public String[] getHands() {
+        return hands;
+    }
 
-
-
-//    public void numberOfEachTypeOfHand() {
-//        int fiveOfAKind = 0;
-//        int fullHouses = 0;
-//        int fourOfAKind = 0;
-//        int threeOfAKind = 0;
-//        int twoPair = 0;
-//        int onePair = 0;
-//        int highCard = 0;
-//
-//        System.out.println("Number of five of a kind hands: " + fiveOfAKind);
-//        System.out.println("Number of full house hands: " + fullHouses);
-//        System.out.println("Number of four of a kind hands: " + fourOfAKind);
-//        System.out.println("Number of three of a kind hands: " + threeOfAKind);
-//        System.out.println("Number of two pair hands: " + twoPair);
-//        System.out.println("Number of one pair hands: " + onePair);
-//        System.out.println("Number of high card hands: " + highCard);
-//        System.out.println("Total Bid Value: ");
-//
-//    }
-//}
-
-//    public int cardTypes() {
-//        //some preface for this:
-//        //i had to do some research and found that
-//        //set is a java import that takes  in only unique elements
-//        //to convert our array into a set, it seems we must use lists as a bridge to do so
-//
-//        List<String> list = Arrays.asList(hands[0]);
-//        Set<String> uniqueStrings = new HashSet<>(list);
-//        int typeNumber = uniqueStrings.size();
-//
-//        return typeNumber;
-//    }
-
-// WELCOME TO THE GRAVEYARD
-//        String cardIndices = "1";
-//        for (int i = 1; i < cards.length; i++) {
-//            boolean matched = false;
-//            // Check against all previous cards
-//            for (int j = 0; j < i; j++) {
-//                if (cards[i].equals(cards[j])) {
-//                    cardIndices += (j + 1); //
-//                    matched = true;
-//                    break; // pls stop checking more
+//    public void giveRankingsByN(ArrayProject[] frog, int totalRankings) {
+//        for (ArrayProject hand : frog) {
+//            int cardNumber = 0;
+//            String card = hand.getHands()[cardNumber];
+//            int ranksAbove = 0;
+//            for (int i = 0; i < frog.length; i++) {
+//                while (this.getCardValue(card) == this.getCardValue(frog[i].getHands()[i])) {
+//                    cardNumber++;
+//                }
+//                if (this.getCardValue(card) > this.getCardValue(frog[i].getHands()[i])) {
+//                    ranksAbove = cardNumber;
 //                }
 //            }
-//            // If no match was found, use its own index
-//            if (!matched) {
-//                int greatestNum = 0;
-//                for (int i2 = 0; i2 < cardIndices.length(); i2++) {
-//                    if (Integer.parseInt(cardIndices.substring(i2, i2 + 1)) > greatestNum) {
-//                        greatestNum = Integer.parseInt(cardIndices.substring(i2, i2 + 1));
-//                    }
-//                }
-//                cardIndices += (greatestNum + 1);
-//            }
+//            cardNumber++;
+//            hand.setRank(ranksAbove);
 //        }
-//        System.out.println(cardIndices);
-//        int greatestNum = 0;
-//        for (int i = 0; i < cardIndices.length(); i++) {
-//            if (Integer.parseInt(cardIndices.substring(i, i + 1)) > greatestNum) {
-//                greatestNum = Integer.parseInt(cardIndices.substring(i, i + 1));
-//            }
-//        }
-//        if (cards[1].equals(cards[0])) {
-//            cardIndices += "1";
-//        } else {
-//            cardIndices += "2";
-//        }
-//        if (cards[2].equals(cards[0])) {
-//            cardIndices += "1";
-//        } else if (cards[2].equals(cards[1])){
-//            cardIndices += "2";
-//        } else {
-//            cardIndices += "3";
-//        }
-//        if (cards[3].equals(cards[0])) {
-//            cardIndices += "1";
-//        } else if (cards[3].equals(cards[1])) {
-//            cardIndices += "2";
-//        } else if (cards[3].equals(cards[2])) {
-//            cardIndices += "3";
-//        } else {
-//            cardIndices += "4";
-//        }
-//        if (cards[4].equals(cards[0])) {
-//            cardIndices += "1";
-//        } else if (cards[4].equals(cards[1])) {
-//            cardIndices += "2";
-//        } else if (cards[4].equals(cards[2])) {
-//            cardIndices += "3";
-//        } else if (cards[4].equals(cards[3])){
-//            cardIndices += "4";
-//        } else {
-//            cardIndices += "5";
-//        }
-//         for (int i = 0; i < cards.length; i++) {
-//            for (int repeater = 0; repeater < cards.length; repeater++)
-//
-//                if (!cards[i].equals(cards[repeater])) {
-//                    typeNumber ++;
-//                }
-//        }
-//        typeNumber = greatestNum;
-//        return typeNumber;
-//    }
-//
-//}
-   /*for (String card : hand) {
-       String numbers = hands.split(",");
+public void giveRankingsByN(ArrayProject[] frog, int offset) {
+    for (ArrayProject hand : frog) {
+        int beatenInGroup = 0;
+
+        for (ArrayProject other : frog) {
+            if (hand == other) continue; // Don't compare to self
+
+            int cardNumber = 0;
+            // Skip cards that are identical (The "Fall Through")
+            while (cardNumber < 4 &&
+                    getCardValue(hand.getHands()[cardNumber]) == getCardValue(other.getHands()[cardNumber])) {
+                cardNumber++;
+            }
+
+            // Compare the first card that is different
+            if (getCardValue(hand.getHands()[cardNumber]) > getCardValue(other.getHands()[cardNumber])) {
+                beatenInGroup++;
+            }
+        }
+        // Rank = (Number of hands you beat in this group) + (Base offset)
+        hand.setRank(beatenInGroup + offset);
+    }
+}
 
 
 
+    public int getRank() {
+        return rank;
+    }
+}
 
-       int[] values = new int[numbers.length];
-       for (int i = 0; i < values.length; i++) {
-           values[i] = Integer.parseInt(numbers[i]);
-       }
-       System.out.println(values[0]);
-*/
